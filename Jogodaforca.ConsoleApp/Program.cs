233aa249fc7ed55a4318f3b4c89b0894eae8f9e8
Alec Luí <dr.elucidator@gmail.com>
2026-03-16 15:45:42 -0300

    first commit

---
+++ Jogodaforca.ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Jogodaforca.ConsoleApp;
+/*
+    1. Ao iniciar o jogo, deve ser selecionada uma palavra aleatória à partir de uma lista. 
+    2. O jogador poderá chutar a palavra secreta letra por letra, cada letra certa deverá ser apresentada, 
+    assim como as letras erradas. 
+    3. O jogador poderá cometer até cinco erros, caso erre pela quinta vez, ou acerte a palavra a partida 
+    acaba. 
+    4. Deve-se apresentar um desenho da forca sendo atualizado a cada erro.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Jogo da Forca");
+
+            string[] palavras = [
+            "ABACATE",
+            "ABACAXI",
+            "ACEROLA",
+            "AÇAÍ",
+            "ARAÇÁ",
+            "ABACATE",
+            "BACABA",
+            "BACURI",
+            "BANANA",
+            "CAJÁ",
+            "CAJU",
+            "CARAMBOLA",
+            "CUPUAÇU",
+            "GRAVIOLA",
+            "GOIABA",
+            "JABUTICABA",
+            "JENIPAPO",
+            "MAÇÃ",
+            "MANGABA",
+            "MANGA",
+            "MARACUJÁ",
+            "MURICI",
+            "PEQUI",
+            "PITANGA",
+            "PITAYA",
+            "SAPOTI",
+            "TANGERINA",
+            "UMBU",
+            "UVA",
+            "UVAIA"
+        ];
+
+            System.Console.WriteLine("Deseja continuar? [s/n]: ");
+            string? operacaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if (operacaoContinuar != "S")
+                break;
+        }
+    }
+}
